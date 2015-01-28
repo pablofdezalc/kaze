d189a8b98b6ce049e9950fefceb1a2fdbd723e51
Dmitry-Me <wipedout@yandex.ru>
2015-01-28 11:38:48 +0300

    Remove unused variable

--- src/lib/utils.cpp
+++ src/lib/utils.cpp
@@ -347,7 +347,6 @@ bool read_homography(const std::string& homography_path, cv::Mat& H1toN) {
   float h31 = 0.0, h32 = 0.0, h33 = 0.0;
   int  tmp_buf_size = 256;
   char tmp_buf[tmp_buf_size];
-  string tmp_string;
 
   // Allocate memory for the OpenCV matrices
   H1toN = cv::Mat::zeros(3,3,CV_32FC1);
